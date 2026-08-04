606b36b68d8330930deab4a0987f6ab8188907e2
Mèo Gaming 2206 <maphucgiapc2ps@gmail.com>
2026-08-04 12:12:27 +0700

    Replace thongBao function with copyScript function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function thongBao() {
-    alert("Xin chào! Bạn vừa bấm vào nút trên trang web.");
+function copyScript() {
+    var copyText = document.getElementById("scriptText");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // Dành cho thiết bị di động
+    
+    // Thực hiện copy vào clipboard
+    navigator.clipboard.writeText(copyText.value);
+    
+    alert("Đã copy script thành công!");
 }
